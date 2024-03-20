29fb591bd60559d6fe82b4f8b9e83ba719948982
vainoikevin <thanhtuankien1503@gmail.com>
2024-03-20 22:34:46 +0700

    Update ai-ask.py

--- commands/slash/ai-ask.py
+++ commands/slash/ai-ask.py
@@ -9,20 +9,20 @@ genai.configure(api_key=ai_key)
 
 model = genai.GenerativeModel('gemini-pro',safety_settings=[
 {
-  "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT"
-  "probability": "NEGLIGIBLE"
+  "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+  "threshold": "BLOCK_NONE",
 },
 {
-  "category": "HARM_CATEGORY_HATE_SPEECH"
-  "probability": "NEGLIGIBLE"
+  "category": "HARM_CATEGORY_HATE_SPEECH",
+  "threshold": "BLOCK_NONE",
 },
 {
-  "category": "HARM_CATEGORY_HARASSMENT"
-  "probability": "NEGLIGIBLE"
+  "category": "HARM_CATEGORY_HARASSMENT",
+  "threshold": "BLOCK_NONE",
 },
 {
-  "category": "HARM_CATEGORY_DANGEROUS_CONTENT"
-  "probability": "NEGLIGIBLE"
+  "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+  "threshold": "BLOCK_NONE",
 }
 ])
 
