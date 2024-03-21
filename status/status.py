039df6852649e6a9fff823d9b2eb37d06b45adf4
vainoikevin <thanhtuankien1503@gmail.com>
2024-03-21 22:02:54 +0700

    Create status.py

---
+++ status/status.py
@@ -0,0 +1,36 @@
+import discord, aiosqlite
+from discord.ext import commands
+
+
+class rd(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+
+  # @commands.Cog.listener()
+  # async def on_ready(self):
+  #   db = aiosqlite.connect("./db/main.sqlite")
+  #   cursor = db.cursor()
+  #   cursor.execute('CREATE TABLE IF NOT EXISTS main(user_id INTEGER, wallet INTEGER, bank INTEGER)')
+
+  # @commands.Cog.listener()
+  # async def on_message(self, message):
+  #   if message.author.bot:
+  #     return
+  
+  #   author = message.author
+  #   db = aiosqlite.connect('./db/main.main.sqlite')
+  #   cursor = db.cursor()
+  #   cursor.execute(f'SELECT user_id FROM main WHERE user_id ={author.id}')
+  #   result = cursor.fetchone()
+    
+  #   if result is None:
+  #     sql = ('INSERT INTO main(user_id, wallet, bank) VALUES (?,?,?)')
+  #     val = (author.id,0,0)
+  #     cursor.execute(sql,val)
+
+  #   db.comit()
+  #   cursor.close()
+  #   db.close()
+async def setup(bot):
+  await bot.add_cog(rd(bot))
