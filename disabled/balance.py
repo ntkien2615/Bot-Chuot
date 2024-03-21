4f5ab12a8f6208ee08aae0f1146b1ecc008332fd
vainoikevin <thanhtuankien1503@gmail.com>
2024-03-21 22:23:03 +0700

    modified:   disabled/balance.py

--- disabled/balance.py
+++ disabled/balance.py
@@ -1,20 +1,20 @@
-# import discord,aiosqlite
-# from discord import app_commands
-# from discord.ext import commands
+import discord,aiosqlite
+from discord import app_commands
+from discord.ext import commands
 
 
-# class balance(commands.Cog):
-#     def __init__(self,bot):
-#         self.bot = bot
+class balance(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
 
-#     @app_commands.command(name="balance",description="xem thử ví bạn (người khác) còn bao nhiêu")
-#     async def balance(self, interaction:discord.Interaction,member: discord.Member = None):
-#         if member == None:
-#             member = interaction.user
+    @app_commands.command(name="balance",description="xem thử ví bạn (người khác) còn bao nhiêu")
+    async def balance(self, interaction:discord.Interaction,member: discord.Member = None):
+        if member == None:
+            member = interaction.user
 
-#         db = aiosqlite.connect("./db/main.sqlite")
-#         cursor = db.cursor()
+        db = aiosqlite.connect("./db/main.sqlite")
+        cursor = db.cursor()
 
-#         cursor.execute()
+        cursor.execute()
 
 
