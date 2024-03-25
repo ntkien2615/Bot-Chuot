faf3dfa64856e4057d93239312ac3e2d2aef2a7b
vainoikevin <thanhtuankien1503@gmail.com>
2024-03-25 22:35:30 +0700

    modified:   commands/slash/aiask.py

--- commands/slash/aiask.py
+++ commands/slash/aiask.py
@@ -15,26 +15,25 @@ class aiask(commands.Cog):
         if not question or len(question) > 200:
             await interaction.response.send_message("Câu hỏi không hợp lệ.")
             return
-    
-    load_dotenv(find_dotenv())
-    ai_api_key = os.getenv("ai_api_key")
-    genai.configure(api_key=ai_api_key)
-    model = genai.GenerativeModel('gemini-pro',safety_settings=[
-        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-])
+        
+        load_dotenv(find_dotenv())
+        ai_api_key = os.getenv("ai_api_key")
+        genai.configure(api_key=ai_api_key)
+        model = genai.GenerativeModel('gemini-pro',safety_settings=[
+            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+            {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        ])
 
+        try:
+            reply = await model.generate_content(question)
+        except Exception as e:
+            print(f"Lỗi khi gọi API: {e}")
+            await interaction.response.send_message("Lỗi hệ thống. Vui lòng thử lại sau.")
+            return
 
-    try:
-        reply = model.generate_content(question)
-    except Exception as e:
-        print(f"Lỗi khi gọi API: {e}")
-        await interaction.response.send_message("Lỗi hệ thống. Vui lòng thử lại sau.")
-        return
-
-    await interaction.response.send_message(f'{reply.text}')
+        await interaction.response.send_message(f'{reply.text}')
 
 async def setup(bot):
   await bot.add_cog(aiask(bot))
