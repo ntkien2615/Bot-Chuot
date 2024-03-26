c0917be686626a5df7d8cbe67f3d23a6543c22a4
vainoikevin <thanhtuankien1503@gmail.com>
2024-03-26 22:26:43 +0700

    modified:   commands/slash/balance.py

--- commands/slash/balance.py
+++ commands/slash/balance.py
@@ -8,15 +8,7 @@ class balance(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="balance",description="xem thử ví bạn (người khác) còn bao nhiêu")
-    async def balance(self, interaction:discord.Interaction,member: discord.Member = None):
-        if member == None:
-            member = interaction.user
-        await open_acount(member)
-        users = get_bank_data()
-        wallet = users[str(user.id)]["wallet"]
-        bank = users[str(user.id)]["bank"]
-        await interaction.response.send_message(f'{wallet} -- {bank}')
-    async def open_acount(user):
+    async def open_account(user):
         users = get_bank_data()
         if str(user.id) in users:
             return False
@@ -31,6 +23,14 @@ class balance(commands.Cog):
     async def get_bank_data():
         with open("./balance/balance.json","r") as f:
             users = json.load(f)
+    async def balance(self, interaction:discord.Interaction,member: discord.Member = None):
+        if member == None:
+            member = interaction.user
+        await open_account(member)
+        users = await get_bank_data()
+        wallet = users[str(user.id)]["wallet"]
+        bank = users[str(user.id)]["bank"]
+        await interaction.response.send_message(f'{wallet} -- {bank}')
 async def setup(bot):
     await bot.add_cog(balance(bot))
 
