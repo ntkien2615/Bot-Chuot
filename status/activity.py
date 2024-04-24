25db5e7a32db659ea8c2d3ad37e826509b0dc742
vainoikevin <thanhtuankien1503@gmail.com>
2024-04-24 20:23:08 +0700

    modified:   status/activity.py

--- status/activity.py
+++ status/activity.py
@@ -8,30 +8,9 @@ class actvity(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.status = cycle([
-            'Chẳng giữ những lời hứa, lúc xưa từng hẹn thề',
-            'Đón đưa cùng người về, những nơi mà ta êm ấm',
-            'Lời chia tay em nói, thế thôi đành ngậm ngùi',
-            'Lẻ loi nhìn người yên vui cùng ai',
-            'Ngồi ôm bao nỗi nhớ, ngẩn ngơ vẫn đợi chờ',
-            'Giấc mơ lụi tàn, để con tim lặng im vỡ nát',
-            'Giờ hai ta hai hướng, nhớ thương còn lại gì',
-            'Vấn vương cũng chẳng níu em đừng đi !',
-            'Tại sao anh còn thương em mãi',
-            'Nhưng lòng đau thì ai có hay ?',
-            'Người đi người buông ai thấu đâu',
-            'Khi tình ta giờ chia hai ngã',
-            'Là em đã rời xa anh đấy',
-            'Để tình tan còn bao đắng cay',
-            'Chẳng câu biệt ly em bước đi',
-            'Theo người ta em không suy nghĩ',
-            'Vì ai em vội quên năm tháng',
-            'Kỷ niệm xưa giờ đây trái ngang',
-            'Để cho nước mắt còn rơi trên',
-            'Đôi bờ mi mà sao em nỡ ?',
-            'Lòng còn vương đem lời thương chôn giấu',
-            'Bao đậm sâu giờ cũng đớn đau',
-            'Đành tâm nhìn em hạnh phúc',
-            'Những ngọt ngào em trao cho người ta !'
+            "Ú òa!",
+            "Ăn mừng những lời nói dối của Thiệu",
+            "Hôm nào cũng là 7 tháng 3"
         ])
 
     @tasks.loop(seconds=5.0)
