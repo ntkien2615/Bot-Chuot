a963e1fa79339012b8ae6b672d62e0ef74ed03bf
vainoikevin <thanhtuankien1503@gmail.com>
2024-06-16 21:47:18 +0700

    new file:   commands/slash/message_delete.py

---
+++ commands/slash/message_delete.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class delslash(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='msg_delete', description='Xóa tin nhắn')
+    @commands.has_permissions(manage_messages=True)
+    async def msgdeleteslash(self,interaction: discord.Interaction,ammount: int):
+        try:
+            if amount > 100:
+                await interaction.response.send_message("Bro tôi không xóa nổi 100 tin nhắn đâu", ephemeral=True)
+            return
+
+            await interaction.channel.purge(limit=amount, check=lambda msg: not msg.pinned)
+            embed = discord.Embed(title="", description=f"{ammount} tin nhắn đã boom by A-train", color=discord.Color.random())
+            embed.set_image(url="https://tenor.com/view/a-train-edit-a-train-the-boys-a-train-the-boys-edit-gif-26341809")
+            await interaction.send_message(Embed=embed, ephemeral=True)
+        except Exception as e:
+            print(e)
+
+
+async def setup(bot):
+    await bot.add_cog(pingslash(bot))
