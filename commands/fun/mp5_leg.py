2187d42c32b511bcfadd104461aa920284e98953
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-26 22:00:21 +0700

    new file:   commands/fun/mp5_leg.py 	new file:   img/yukka.jpg

---
+++ commands/fun/mp5_leg.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class legslash(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='mp5_leg', description='cẩn thận cặp giò của m đấy')
+    @app_commands.describe(member='Chọn cái đứa nào ây')
+    async def dice(self, interaction: discord.Interaction, member: discord.Member):
+        if member == None:
+            await interaction.response.send_message('Well, ko có ai làm sao có cặp giò', ephemeral=True)
+            return
+        elif member == interaction.user:
+            await interaction.response.send_message(f"Thiệt luôn, mà cặp giò của bạn trông khá múp ấy {member}")
+        else:
+            await interaction.response.send_message(f"CAN THAN CAP GIO VOI HON DAI CUA M DAY <@{member}>")
+            await interaction.message.add_files(files="./img/yukka.jpg")
+
+async def setup(bot):
+    await bot.add_cog(legslash(bot))
+d
