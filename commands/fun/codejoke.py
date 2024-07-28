5a75c0fa26c161e71baa89d369a9c29d4f49a786
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-28 15:26:17 +0700

    new file:   .vscode/fast cog commands.code-snippets 	new file:   commands/fun/codejoke.py

---
+++ commands/fun/codejoke.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import pyjokes
+
+
+class codejoke(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='codejoke',description='Tạo code joke (chỉ hỗ trợ tiếng anh)') 
+    @app_commands.describe()
+    async def codejoke(self, interaction:discord.Interaction):
+        msg = discord.Embed(title="Coder Joke v1.ABC",
+                            color=discord.Colour.random())
+        msg.add_field(name="",
+                      value=pyjokes.get_joke(
+                          language='en', category='neutral'),
+                      inline=False)
+        await interaction.response.send_message(embed=embed)
+async def setup(bot):
+    await bot.add_cog(codejoke(bot))
