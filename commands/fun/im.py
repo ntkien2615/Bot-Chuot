72869199734ae75877e3fc3dcacb156980a08681
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-28 10:17:04 +0700

    new file:   commands/fun/im.py 	modified:   commands/fun/trongtruonghop.py

---
+++ commands/fun/im.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Im(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="im",description="im nao co be")
+    @app_commands.describe(member="Nhập tên người bạn muốn bắt im")
+    async def Im(self, interaction:discord.Interaction, member:discord.Member):
+        if (member == None) or (member == interaction.user):
+            await interaction.response.send_message('Nhập người nào vào đi',ephemeral=True)
+            return
+        else:
+            embed = discord.Embed(title="", description="", color=discord.Color.random())
+            embed.set_image(url="https://media.discordapp.net/attachments/883268139922636820/1125768473595875338/e6328ff4069e2184f8f212fd692ce117.png")
+            await interaction.response.send_message(f"<@{member.id}> Im nào cô bé của tui", embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Im(bot))
--- commands/fun/trongtruonghop.py
+++ commands/fun/trongtruonghop.py
@@ -10,9 +10,9 @@ class Trongtruonghop(commands.Cog):
     @app_commands.command(name='trong_truong_hop', description='trong trường hợp nhóm này')
 
     async def Trongtruonghop(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="", description=f'Tôi tên là {interaction.user}. Trong trường hợp nhóm này bị điều tra bởi các cơ quan trực thuộc bộ công an (hoặc các tổ chức chính trị tương tự phục vụ cho nhà nước CHXHCNVN), tôi khẳng định mình không liên quan tới nhóm hoặc những cá nhân khác trong nhóm này. Tôi không rõ tại sao mình lại có mặt ở đây vào thời điểm này, có lẽ tài khoản của tôi đã được thêm bởi một bên thứ ba. Tôi cũng xin khẳng định rằng mình không hề giúp sức cho những hành động chống phá Đảng và nhà nước của các thành viên trong nhóm này. Xin cảm ơn!', color=discord.Color.red())
+        embed = discord.Embed(title="", description=f'Tôi tên là {interaction.user}. Trong trường hợp nhóm này bị điều tra bởi các cơ quan trực thuộc bộ công an (hoặc các tổ chức chính trị tương tự phục vụ cho nhà nước CHXHCNVN), tôi khẳng định mình không liên quan tới nhóm hoặc những cá nhân khác trong nhóm này. Tôi không rõ tại sao mình lại có mặt ở đây vào thời điểm này, có lẽ tài khoản của tôi đã được thêm bởi một bên thứ ba. Tôi cũng xin khẳng định rằng mình không hề giúp sức cho những hành động chống phá Đảng và nhà nước của các thành viên trong nhóm này. Xin cảm ơn!', color=discord.Color.random())
         embed.set_image(url="https://th.bing.com/th/id/OIP.3I9zIQg4SUu9pMO9vZcosQHaEK?rs=1&pid=ImgDetMain")
         await interaction.response.send_message(embed=embed)
 
-def setup(bot):
-    bot.add_cog(Trongtruonghop(bot))
+async def setup(bot):
+    await bot.add_cog(Trongtruonghop(bot))
