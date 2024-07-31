275faef06fce9b6fcb69d09e89ae517595dc8b64
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-31 15:38:29 +0700

    hack.py

--- commands/fun/hack.py
+++ commands/fun/hack.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import asyncio
+import random
+
 class Hack(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -13,12 +15,33 @@ class Hack(commands.Cog):
             await interaction.response.send_message("Ông không thể hack chính ông đc, thử đứa khác đi")
             return
 
-        await interaction.response.send_message(f"Initiating playful infiltration into <@{user.id}>'s system...")
+        await interaction.response.send_message(f"Bắt đầu thực hiện việc hack nguy hiểm vào máy tính của <@{user.id}>...")
+        await asyncio.sleep(1)
 
         for i in range(5, 0, -1):
-            await interaction.edit_original_response(content=f"Hacking progress: {i} seconds remaining...")
+            await interaction.edit_original_response(content=f"Tìm kiếm wifi của <@{user.id}: {i} seconds remaining...")
             await asyncio.sleep(1)  # Use asyncio.sleep for non-blocking delays
 
+        await interaction.edit_original_response(content=f'Phát hiện wifi của <@{user.id}>')
+        await asyncio.sleep(1)
+
+        def random_file_read(self, file_path):
+        try:
+            with open(file_path, "r") as f:
+                lines = f.readlines()
+            if lines:
+                return random.choice(lines).strip()
+        except (IndexError, FileNotFoundError) as e:
+            return None
+
+        wifi_name = random_file_read('./txt_files/hack/1a_wifiname.txt')
+        number_connected = random.randint(1,9)
+        await interaction.edit_original_response(content=f'Tên wifi: {wifi_name}, có {number_connected} kết nối')
+        await interaction.edit_original_response(content=f'Bắt đầu WPA Handshake...')
+        await asyncio.sleep(2)
+        wifi_pass = random_file_read('./txt_files/hack/1b_wifipass.txt')
+        await interaction.edit_original_response(content=f'Thành công, password là: {wifi_pass}')
+        await asyncio.sleep(1)
         # Conclude with a humorous message
         await interaction.edit_original_response(content=f"Hack complete! <@{user.id}>'s computer is now filled with... confetti! ")
 
