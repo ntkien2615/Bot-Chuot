4a5c629faaa363b066ea6263a79cd0648c2524d2
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-31 16:08:17 +0700

    hack.py

--- commands/fun/hack.py
+++ commands/fun/hack.py
@@ -7,6 +7,15 @@ import random
 class Hack(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    def random_file_read(self, file_path):
+            try:
+                with open(file_path, "r") as f:
+                    lines = f.readlines()
+                if lines:
+                    return random.choice(lines).strip()
+            except (IndexError, FileNotFoundError) as e:
+                return None
 
     @app_commands.command(name='hack',description='hack vào máy ai đó')
     @app_commands.describe(user='máy tính của ai')
@@ -24,15 +33,6 @@ class Hack(commands.Cog):
 
         await interaction.edit_original_response(content=f'Phát hiện wifi của <@{user.id}>')
         await asyncio.sleep(1)
-
-        def random_file_read(self, file_path):
-            try:
-                with open(file_path, "r") as f:
-                    lines = f.readlines()
-                if lines:
-                    return random.choice(lines).strip()
-            except (IndexError, FileNotFoundError) as e:
-                return None
 
         wifi_name = self.random_file_read('./txt_files/hack/1a_wifiname.txt')
         number_connected = random.randint(1,9)
