73d4e3bbaa7d7228e6754c146b39654398d57ebf
vainoikevin <thanhtuankien1503@gmail.com>
2024-07-31 09:37:16 +0700

    hack.py

---
+++ commands/fun/hack.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import time
+class Hack(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='hack',description='hack vào máy ai đó')
+    @app_commands.describe(user='máy tính của ai')
+    async def hack(self,interaction:discord.Interaction,user=discord.Member):
+        second == 5
+        await interaction.response.send_message(f'Bắt đầu tiến hành cuộc tấn công nguy hiểm vào máy tính của <@{user.id}> trong {second}s')
+        for i in range(1,5):
+            second -= 1
+            await interaction.response.edit_message(f'Bắt đầu tiến hành cuộc tấn công nguy hiểm vào máy tính của <@{user.id}> trong {second}s')
+            time.sleep(1)        
+        await interaction.response.edit_message(f'đã hoàn thành việc hack vào máy tính của  <@{user.id}>')
+
+async def setup(bot):
+    await bot.add_cog(Hack(bot))
