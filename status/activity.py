1251f925fcde4d9c418c1b8dbc859d92f67d721d
vainoikevin <thanhtuankien1503@gmail.com>
2024-08-05 21:48:32 +0700

    modified:   status/activity.py

--- status/activity.py
+++ status/activity.py
@@ -5,22 +5,21 @@ from itertools import cycle
 
 class actvity(commands.Cog):
 
+    def read_file_into_list(self, filename):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+        return lines
+
     def __init__(self, bot):
         self.bot = bot
-        self.status_list = self.read_file_into_list("./txt_files/activity.txt")
-        self.status_cycle = cycle(self.status_list)
-
-        def read_file_into_list(filename):
-            with open(filename, 'r') as file:
-                lines = file.readlines()
-            return lines
+        self.status = cycle(self.read_file_into_list("./txt_files/activity.txt"))
 
     @tasks.loop(seconds=5.0)
     async def change_status(self):
         await self.bot.change_presence(status=discord.Status.online,
                                        activity=discord.Activity(
                                            type=discord.ActivityType.watching,
-                                           name=next(self.status_cycle)))
+                                           name=next(self.status)))
 
     @commands.Cog.listener()
     async def on_ready(self):
