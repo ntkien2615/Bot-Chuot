4491d8b898e153275dac183ff0a3041544b1cba5
vainoikevin <thanhtuankien1503@gmail.com>
2024-08-11 20:04:48 +0700

    new file:   commands/fun/momoi.py 	new file:   txt_files/moimoi.txt

---
+++ commands/fun/momoi.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class moimoi(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def random_file_read(self, file_path):
+            try:
+                with open(file_path, "r") as f:
+                    lines = f.readlines()
+                if lines:
+                    return random.choice(lines).strip()
+            except (IndexError, FileNotFoundError) as e:
+                return None
+
+    @app_commands.command(name='momoi',description='momoi ní...') 
+    @app_commands.describe(user='Người muốn được đua xe')
+    async def moimoi(self, interaction:discord.Interaction,user=discord.Member):        
+        momoi = self.random_file_read('./txt_files/moimoi.txt')
+        embed = discord.Embed(title="", description="", color=discord.Color.random())
+        embed.set_image(url=momoi)
+
+        await interacton.response.send_message(f'{user}',embed=embed)
+async def setup(bot):
+    await bot.add_cog(moimoi(bot))
