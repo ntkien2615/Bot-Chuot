06e3f30e34daef03c91746b19984abc0ee1aad46
vainoikevin <thanhtuankien1503@gmail.com>
2024-08-12 08:03:25 +0700

    modified:   commands/fun/momoi.py

--- commands/fun/momoi.py
+++ commands/fun/momoi.py
@@ -20,7 +20,7 @@ class momoi(commands.Cog):
     @app_commands.command(name='momoi',description='momoi ní...') 
     @app_commands.describe(user='Người muốn được đua xe')
     async def moimoi(self, interaction:discord.Interaction, user:discord.Member):
-        if (member == None) or (member == interaction.user):
+        if (userr == None) or (user == interaction.user):
             await interaction.response.send_message('Nhập người nào vào đi',ephemeral=True)
             return
         else:      
