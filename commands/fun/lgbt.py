81016a0d12a06c91714b02733f070aabe795f4c3
vainoikevin <thanhtuankien1503@gmail.com>
2024-09-04 22:31:55 +0700

    new file:   commands/fun/lgbt.py 	new file:   txt_files/lgbt.txt

---
+++ commands/fun/lgbt.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+
+
+class Lgbt(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def random_file_read(self, file_path):
+            try:
+                with open(file_path, "r") as f:
+                    lines = f.readlines()
+                if lines:
+                    return random.choice(lines).strip()
+            except (IndexError, FileNotFoundError) as e:
+                return None
+    
+    @app_commands.command(name='lgbt',description='lgbt?') 
+    @app_commands.describe(user='Người bạn nghi là gay')
+    async def moimoi(self, interaction: discord.Interaction, user:discord.Member):
+        if (user == None) or (user == interaction.user):
+            await interaction.response.send_message('Nhập người nào vào đi',ephemeral=True)
+            return
+        else:      
+            lgbt = self.random_file_read('./txt_files/lgbt.txt')
+            embed = discord.Embed(title="", description="", color=discord.Color.random())
+            embed.set_image(url=lgbt)
+
+        await interaction.response.send_message(f"<@{user.id}>, 🏳️‍🌈?!?!",embed=embed)
+
+    
+
+async def setup(bot):
+    await bot.add_cog(Lgbt(bot))
