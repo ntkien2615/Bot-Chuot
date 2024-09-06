991239570bd2b0f629b402bfc22f9a7cf87237e3
vainoikevin <thanhtuankien1503@gmail.com>
2024-09-06 17:51:29 +0700

    modified:   commands/general/avatar.py

--- commands/general/avatar.py
+++ commands/general/avatar.py
@@ -2,41 +2,39 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-class MyView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-
-    @discord.ui.button(label='test', style=discord.ButtonStyle.grey)
-    async def asdf(self, interaction: discord.Interaction, button: discord.ui.Button,member: discord.Member):
-        embed_msg = discord.Embed(title=f"Avatar Local của {member}",
-                                  color=discord.Color.random())
-        embed_msg.set_author(name=f"{member}",
-                             icon_url=member.guild_avatar)
-        embed_msg.set_image(url=member.guild_avatar)
-        embed_msg.set_footer(text=f"Bởi {interaction.user}",
-                             icon_url=interaction.user.avatar)
-        await interaction.response.edit_message(content='test success',embed=embed_msg)
-
-
 class avatarslash(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name='avatar', description='lấy avatar của bạn hoặc người khác')
-    async def avtslash(self, interaction: discord.Interaction, member: discord.Member = None):
+    @app_commands.choices(options=[
+        discord.app_commands.Choice(name="Global", value="global"),
+        discord.app_commands.Choice(name="Local", value="local")
+    ])
+    async def avtslash(self, interaction: discord.Interaction, options: str, member: discord.Member = None):
         if member == None:
             member = interaction.user
 
-        view = MyView()
-        embed_msg = discord.Embed(title=f"Avatar Global của {member}",
-                                  color=discord.Color.random())
-        embed_msg.set_author(name=f"{member}",
-                             icon_url=member.avatar)
-        embed_msg.set_image(url=member.avatar)
-        embed_msg.set_footer(text=f"Bởi {interaction.user}",
-                             icon_url=interaction.user.avatar)
-        await interaction.response.send_message(embed=embed_msg, view=view)
+        if options == 'global':
+            embed_msg = discord.Embed(title=f"Avatar Global của {member}",
+                                    color=discord.Color.random())
+            embed_msg.set_author(name=f"{member}",
+                                icon_url=member.avatar)
+            embed_msg.set_image(url=member.avatar)
+            embed_msg.set_footer(text=f"Bởi {interaction.user}",
+                                icon_url=interaction.user.avatar)
+            await interaction.response.send_message(embed=embed_msg, view=view)
+        
+        if options == 'local':
+            embed_msg = discord.Embed(title=f"Avatar Local của {member}",
+                                    color=discord.Color.random())
+            embed_msg.set_author(name=f"{member}",
+                                icon_url=member.avatar)
+            embed_msg.set_image(url=member.guild_avatar.url)
+            embed_msg.set_footer(text=f"Bởi {interaction.user}",
+                                icon_url=interaction.user.avatar)
+            await interaction.response.send_message(embed=embed_msg, view=view)
 
 
 async def setup(bot):
