ae6c60152272d9e863556a0ecf061a7cd62d61b1
vainoikevin <thanhtuankien1503@gmail.com>
2025-01-17 00:55:39 +0700

    modified:   keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -18,8 +18,10 @@ def run():
 
 
 def ping(target, debug):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     while (True):
-        r = requests.get(target)
+        r = requests.get(target, headers=headers)
         if (debug == True):
             print("Status Code: " + str(r.status_code))
         time.sleep(random.randint(
