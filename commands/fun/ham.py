1bf3121d79d3a7133fca755cc840f0fa98541d5d
vainoikevin <thanhtuankien1503@gmail.com>
2025-01-25 14:38:07 +0700

    modified:   commands/fun/ham.py

--- commands/fun/ham.py
+++ commands/fun/ham.py
@@ -13,7 +13,6 @@ class ham(commands.Cog):
     async def ham(self, interaction: discord.Interaction, user:discord.Member):
         if user == None:
             interaction.user = user
-        match = random.randint(0,100)
         embed = discord.Embed(title="ham", description=f'🔥 {user.mention}', color=discord.Color.random())
         embed.set_image(url="https://cdn.discordapp.com/emojis/1029699276953100298.webp?size=128&animated=true")
         await interaction.response.send_message(embed=embed)
