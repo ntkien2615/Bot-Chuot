b77887aa5f6a5ac1c5067d6086d7b9dbd320b1c0
vainoikevin <thanhtuankien1503@gmail.com>
2025-03-24 08:24:34 +0700

    modified:   disabled/pvp.py

--- disabled/pvp.py
+++ disabled/pvp.py
@@ -3,6 +3,21 @@ from discord.ext import commands
 from discord import app_commands
 import asyncio
 
+def game_board():
+    pass
+
+def phy_atk():
+    pass
+
+def mage_atk():
+    pass
+
+def shield_add():
+    pass
+
+def run_game():
+    pass
+
 class Pvp(commands.Cog):
 
     def __init__(self, bot):
@@ -16,5 +31,12 @@ class Pvp(commands.Cog):
         else:
             await interaction.response.send_message(f'{opponent.mention}, bạn nhận được lời thách đấu từ {interaction.user.mention}!, hãy bấm vào ô tick để tiếp tục')
             await interaction.message.add_reaction('✅')
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=lambda reaction, user: user == opponent and reaction.message == interaction.message and reaction.emoji == '✅')
+                await interaction.response.send_message(f'{opponent.mention} đã chấp nhận lời thách đấu, hãy bắt đầu!')
+                await run_game()
+            except asyncio.TimeoutError:
+                await interaction.response.send_message(f'Có lẽ {opponent.mention} đã chuồn')
+
 
 
