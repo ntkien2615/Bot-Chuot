434723dda2d29a2a9361e007bb956561f6bfdf32
vainoikevin <thanhtuankien1503@gmail.com>
2025-05-27 08:43:24 +0700

    modified:   commands/general/avatar.py

--- commands/general/avatar.py
+++ commands/general/avatar.py
@@ -17,27 +17,33 @@ class avatarslash(commands.Cog):
             member = interaction.user
 
         if options == 'global':
+            avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
             embed_msg = discord.Embed(title=f"Avatar global của {member}",
                                     color=discord.Color.random())
             embed_msg.set_author(name=f"{member}",
-                                icon_url=member.avatar)
-            embed_msg.set_image(url=member.avatar)
+                                icon_url=avatar_url)
+            embed_msg.set_image(url=avatar_url)
             embed_msg.set_footer(text=f"Bởi {interaction.user}",
-                                icon_url=interaction.user.avatar)
+                                icon_url=interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url)
             await interaction.response.send_message(embed=embed_msg)
         
         if options == 'local':
             try:
+                if member.guild_avatar is None:
+                    await interaction.response.send_message("Người này không có avatar trong máy chủ, có thể người này quá nghèo để có nitro", ephemeral=True)
+                    return
+                
+                avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
                 embed_msg = discord.Embed(title=f"Avatar local của {member}",
                                     color=discord.Color.random())
                 embed_msg.set_author(name=f"{member}",
-                                    icon_url=member.avatar)
+                                    icon_url=avatar_url)
                 embed_msg.set_image(url=member.guild_avatar.url)
                 embed_msg.set_footer(text=f"Bởi {interaction.user}",
-                                    icon_url=interaction.user.avatar)
+                                    icon_url=interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url)
                 await interaction.response.send_message(embed=embed_msg)
-            except AttributeError:
-                await interaction.response.send_message("Người này không có avatar trong máy chủ, có thể người này quá nghèo để có nitro", ephemeral=False)
+            except Exception as e:
+                await interaction.response.send_message("Có lỗi xảy ra khi lấy avatar local", ephemeral=True)
                 return
 
 
